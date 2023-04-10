2228ef984dbd7bdc040bab656b96dacb90b4bb1f
Ekram Hossain <ekramhossain117@gmail.com>
2023-04-10 14:40:59 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,20 @@ import './index.css'
 
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Header from './Components/Header';
+import Home from './Components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
     errorElement: <p>error</p>,
-  },
+
+    children:[{
+
+      path: "/",
+      element: <Home></Home>,
+    }]
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
